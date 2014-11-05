db508a21ab6dc4d69a5ff8779970b52d413b40a1
rey <rey@r-28-190-28-172.comp.nus.edu.sg>
2014-11-05 15:59:59 +0800

    Cleanup of useless files

--- top.php
+++ top.php
@@ -1,46 +1,29 @@
 <div id="header-wrapper">
-				<div class="container">
-					<div class="row">
-						<div class="12u">
+	<div class="container">
+		<div class="row">
+			<div class="12u">
+				
+				<!-- Header -->
+				<header id="header">
+					<div class="inner">
 						
-							<!-- Header -->
-								<header id="header">
-									<div class="inner">
-									
-										<!-- Logo -->
-											<h1><a href="index.html" id="logo">Group 72</a></h1>
-										
-										<!-- Nav -->
-											<nav id="nav">
-												<ul>
-													<li><a href="index.html">Home</a></li>
-													<li class="current_page_item"><a href="bookingpage.html">Book Flights</a></li>
-													<li class="#"><a href="admin_login.php">Login</a></li>
-													<li>
-														<a href="">Dropdown</a>
-														<ul>
-															<li><a href="#">Lorem ipsum dolor</a></li>
-															<li><a href="#">Magna phasellus</a></li>
-															<li>
-																<a href="">Phasellus consequat</a>
-																<ul>
-																	<li><a href="#">Lorem ipsum dolor</a></li>
-																	<li><a href="#">Phasellus consequat</a></li>
-																	<li><a href="#">Magna phasellus</a></li>
-																	<li><a href="#">Etiam dolore nisl</a></li>
-																</ul>
-															</li>
-															<li><a href="#">Veroeros feugiat</a></li>
-														</ul>
-													</li>
-													<li><a href="faq.html">FAQ</a></li>
-												</ul>
-											</nav>
-									
-									</div>
-								</header>
-
-						</div>
+						<!-- Logo -->
+						<h1><a href="index.html" id="logo">Group 72</a></h1>
+						
+						<!-- Nav -->
+						<nav id="nav">
+							<ul>
+								<li class="#">><a href="index.html">Home</a></li>
+								<li class="#"><a href="book.php">Book Flights</a></li>
+								<li class="#"><a href="check.php">Check Booking</a></li>
+								<li class="#"><a href="admin_login.php">Admin Login</a></li>
+							</ul>
+						</nav>
+						
 					</div>
-				</div>
+				</header>
+
 			</div>
+		</div>
+	</div>
+</div>
